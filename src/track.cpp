3e1fdd666c10fbc91cb69e64c571ba86116aa02e
Kevin <koderkk@gmail.com>
2019-07-12 18:15:55 -0400

    Bezier curve stuff
    Former-commit-id: e5dabbd1bba8aaa7360a3659d75b54ecfdaa0811

--- src/track.cpp
+++ src/track.cpp
@@ -5,24 +5,43 @@
 
 void Track::onUpdate(Renderer* renderer, Scene* scene, f32 deltaTime)
 {
-    glm::vec3 p1 = { 0, 0, 10 };
-    glm::vec3 p2 = { 0, 30, 10 };
-    glm::vec3 p3 = { 50, 50, 10 };
-    glm::vec3 p4 = { 100, 100, 10 };
-    glm::vec3 prevP = p1;
     glm::vec4 red = glm::vec4(1, 0, 0, 1);
     glm::vec4 orange = glm::vec4(1.0, 0.5, 0, 1);
-    for (f32 t=0.01f; t<=1.f; t+=0.01f)
+    for (auto& c : connections)
     {
-        glm::vec3 p = getPointOnBezierCurve(p1, p2, p3, p4, t);
-        scene->debugDraw.line(p, prevP, red, red);
-        prevP = p;
+        glm::vec3 prevP;
+        for (f32 t=0.f; t<=1.f; t+=0.01f)
+        {
+            glm::vec3 p = getPointOnBezierCurve(
+                    points[c.pointIndexA].position,
+                    points[c.pointIndexA].position + c.handleOffsetA,
+                    points[c.pointIndexB].position + c.handleOffsetB,
+                    points[c.pointIndexB].position, t);
+            if (t > 0.f)
+            {
+                scene->debugDraw.line(p, prevP, red, red);
+            }
+            prevP = p;
+        }
+
+        scene->debugDraw.line(points[c.pointIndexA].position,
+                points[c.pointIndexA].position + glm::vec3(0, 0, 4),
+                red, red);
+        scene->debugDraw.line(points[c.pointIndexB].position,
+                points[c.pointIndexB].position + glm::vec3(0, 0, 4),
+                red, red);
+        scene->debugDraw.line(points[c.pointIndexA].position + c.handleOffsetA,
+                points[c.pointIndexA].position + c.handleOffsetA + glm::vec3(0, 0, 4),
+                orange, orange);
+        scene->debugDraw.line(points[c.pointIndexA].position,
+                points[c.pointIndexA].position + c.handleOffsetA,
+                orange, orange);
+        scene->debugDraw.line(points[c.pointIndexB].position + c.handleOffsetB,
+                points[c.pointIndexB].position + c.handleOffsetB + glm::vec3(0, 0, 4),
+                orange, orange);
+        scene->debugDraw.line(points[c.pointIndexB].position,
+                points[c.pointIndexB].position + c.handleOffsetB,
+                orange, orange);
     }
-    scene->debugDraw.line(p1, p1 + glm::vec3(0, 0, 4), red, red);
-    scene->debugDraw.line(p4, p4 + glm::vec3(0, 0, 4), red, red);
-    scene->debugDraw.line(p2, p2 + glm::vec3(0, 0, 4), orange, orange);
-    scene->debugDraw.line(p1, p2, orange, orange);
-    scene->debugDraw.line(p3, p3 + glm::vec3(0, 0, 4), orange, orange);
-    scene->debugDraw.line(p4, p3, orange, orange);
 }
 
--- src/track.h
+++ src/track.h
@@ -8,6 +8,22 @@
 
 class Track : public Renderable, public Entity
 {
+    struct Point
+    {
+        glm::vec3 position;
+    };
+
+    struct BezierSegment
+    {
+        glm::vec3 handleOffsetA;
+        u32 pointIndexA;
+        glm::vec3 handleOffsetB;
+        u32 pointIndexB;
+    };
+
+    std::vector<Point> points;
+    std::vector<BezierSegment> connections;
+
     glm::vec3 getPointOnBezierCurve(glm::vec3 p0, glm::vec3 p1, glm::vec3 p2, glm::vec3 p3, f32 t) const
     {
         f32 u = 1.f - t;
@@ -18,5 +34,15 @@ class Track : public Renderable, public Entity
         return glm::vec3(u3 * p0 + (3.f * u2 * t) * p1 + (3.f * u * t2) * p2 + t3 * p3);
     }
 
+public:
     void onUpdate(Renderer* renderer, Scene* scene, f32 deltaTime) override;
+    Track()
+    {
+        points.push_back(Point{ glm::vec3(0, 0, 5) });
+        points.push_back(Point{ glm::vec3(50, 0, 5) });
+        connections.push_back(BezierSegment{ glm::vec3(10, 10, 0), 0, glm::vec3(-10, 10, 0), 1 });
+
+        points.push_back(Point{ glm::vec3(80, -50, 5) });
+        connections.push_back(BezierSegment{ glm::vec3(10, -10, 0), 1, glm::vec3(20, -10, 0), 2 });
+    }
 };
